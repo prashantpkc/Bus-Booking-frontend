f3a032de8d16d564e47922582b5f9ff651176eae
prashantpkc <prashantkumarpkc@gmail.com>
2023-04-04 18:20:30 +0530

    routing is done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
+
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+
+  <Routes>
+    <Route path='/login' element={<Login/>}/>
+
+    <Route path='/Register' element={<Signup/>}/>
+  </Routes>
+  </BrowserRouter>
+ 
 );
 
